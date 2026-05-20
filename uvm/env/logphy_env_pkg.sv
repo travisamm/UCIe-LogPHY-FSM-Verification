146826c6cd2475ee8687cfc036aed61ffc9821f5
Travis Miller <travisamm@berkeley.edu>
2026-05-20 15:19:02 -0700

    file path preserved for the Makefile; payload is now , importing logphy_agent_pkg (back-compat types) and sbinit_agent_pkg (new components).

--- uvm/env/logphy_env_pkg.sv
+++ uvm/env/logphy_env_pkg.sv
@@ -1,15 +1,22 @@
+// Compiled by the Makefile under its historical filename
+// (./env/logphy_env_pkg.sv). The actual package payload is sbinit_env_pkg.
+
 `ifndef LOGPHY_ENV_PKG_SV
 `define LOGPHY_ENV_PKG_SV
 
-package logphy_env_pkg;
+package sbinit_env_pkg;
   import uvm_pkg::*;
   `include "uvm_macros.svh"
-
   import logphy_agent_pkg::*;
+  import sbinit_agent_pkg::*;
+  import sbinit_msg_pkg::*;
 
-  `include "logphy_scoreboard.sv"
+  `include "sbinit_env_cfg.sv"
+  `include "sbinit_virtual_sequencer.sv"
   `include "../coverage/sbinit_coverage.sv"
-  `include "logphy_env.sv"
+  `include "sbinit_scoreboard.sv"
+  `include "sbinit_env.sv"
 
 endpackage
+
 `endif
