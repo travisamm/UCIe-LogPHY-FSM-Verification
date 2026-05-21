8b820b3287db35f804c484261b6e736a23fadc0f
Travis Miller <travisamm@berkeley.edu>
2026-05-20 18:05:46 -0700

    Merge mbtrain coverage from dev-verification branch.

--- uvm/env/mbtrain_env.sv
+++ uvm/env/mbtrain_env.sv
@@ -6,6 +6,7 @@ class mbtrain_env extends uvm_env;
 
   mbtrain_agent      agent;
   mbtrain_scoreboard scoreboard;
+  mbtrain_coverage  coverage;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
@@ -15,11 +16,13 @@ class mbtrain_env extends uvm_env;
     super.build_phase(phase);
     agent      = mbtrain_agent::type_id::create("agent", this);
     scoreboard = mbtrain_scoreboard::type_id::create("scoreboard", this);
+    coverage   = mbtrain_coverage::type_id::create("coverage", this);
   endfunction
 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     agent.monitor.item_collected_port.connect(scoreboard.item_collected_export);
+    agent.monitor.item_collected_port.connect(coverage.analysis_export);
   endfunction
 
 endclass
